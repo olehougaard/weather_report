36fc68e4927bc114a70e20b748992ca752a52aed
OIH <OIH@VIA.DK>
2019-10-11 09:38:27 +0200

    Accepts User-Agent header for browser compatibility

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('static'))
 app.use(body_parser.json())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, User-Agent");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH");
     next();
 });
