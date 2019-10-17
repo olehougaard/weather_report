459a32bff448e7ef7fe83412264201e9a4146431
BarbaraOzar <barbara.ozar@gmail.com>
2019-10-17 14:50:44 +0200

    displaying the rest of info

--- static/model.js
+++ static/model.js
@@ -1,8 +1,7 @@
-function digest({ data }) {
-    const ms_in_day = 24 * 60 * 60 * 1000;
-    const now = new Date();
-    now.setHours(0,0,0,0)
-    const five_days_ago = new Date(now - 5 * ms_in_day)
+function digest({ data, forecast }) {
+    const ms_in_day = 24 * 60 * 60 * 1000
+    const now = new Date()
+    const five_days_ago = new Date(now.setHours(0,0,0,0) - 5 * ms_in_day)
     const temp = []
     const prec = []
     const wind = []
@@ -12,18 +11,25 @@ function digest({ data }) {
         switch(item.type) {
             case 'temperature':
                 temp.push(item)
+                break
             case 'precipitation':
                 prec.push(item)
+                break
             case 'wind speed':
                 wind.push(item)
+                break
             case 'cloud coverage':
                 cloud.push(item)
+                break
         }
     })
 
     const find_latest = array => array.reduce((prev, curr) => prev.time > curr.time ? prev : curr)
     const find_smallest = array => array.reduce((prev, curr) => prev.value < curr.value ? prev : curr)
     const find_largest = array => array.reduce((prev, curr) => prev.value > curr.value ? prev : curr)
+    const last_five_days = array => array.filter(item => new Date(item.time) >= five_days_ago)
+    const sum = array => array.reduce((acc, curr) => acc += curr.value, 0)
+    const round = value => Math.round(value * 100) / 100
 
     const latest_measurements = () => {
         const latest_temp = find_latest(temp)
@@ -33,10 +39,21 @@ function digest({ data }) {
         return [latest_temp, latest_prec, latest_wind, latest_cloud]
     }
 
-    const min_temperature = () => [ find_smallest(temp.filter(item => new Date(item.time) >= five_days_ago)) ]
-    const max_temperature = () => [ find_largest(temp.filter(item => new Date(item.time) >= five_days_ago)) ]
+    const min_temperature = () => find_smallest(last_five_days(temp))
+    const max_temperature = () => find_largest(last_five_days(temp))
+    const total_precipitation = () => [ round(sum(last_five_days(prec))), prec[0].unit ]
+    const average_wind_speed = () => [ round( sum(last_five_days(wind)) / last_five_days(wind).length ), wind[0].unit ]
+    
+    const dominant_direction = () => {
+        const directions = wind.reduce((acc, item) => ({...acc, [item.direction]: acc[item.direction] + 1}),
+        {North: 0, Northeast: 0, East: 0, Southeast: 0, South: 0, Southwest: 0, West: 0, Northwest: 0})
+        const largest = Object.values(directions).reduce((prev, curr) => prev > curr ? prev : curr)
+        return Object.keys(directions).find(key => directions[key] === largest)
+    }
+    
+    const average_cloud_coverage = () => [ round( sum(last_five_days(cloud)) / last_five_days(cloud).length ), cloud[0].unit ]
+    const hourly_predictions = () => forecast
 
-
-
-    return { latest_measurements, min_temperature, max_temperature }
+    return { latest_measurements, min_temperature, max_temperature, total_precipitation, average_wind_speed, dominant_direction, 
+        average_cloud_coverage, hourly_predictions }
 }
--- static/view.js
+++ static/view.js
@@ -1,29 +1,58 @@
-function view(model) {
-    const {div, h2, table, tbody, thead, tr, td} = React.DOM;
+function view(city, model) {
+    const {div, h2, h3, p, table, tbody, thead, tr, td} = React.DOM;
     
     function display_weather_data(data) {
         return table(null, 
-            thead(null, null, tr(null, null, td(null, "Type"), td(null, "Value"), td(null, "Unit"), td(null, "Place"), td(null, "Time"))),
-            tbody(null, null, ...data.map(m => {
+            thead(null, null, tr(null, null, td(null, "Type"), td(null, "Value"), td(null, "Unit"), td(null, "Time"))),
+            tbody(null, null, ...data.map(d => {
                 return tr(null, null, 
-                    td(null, null, m.type),
-                    td(null, null, m.value),
-                    td(null, null, m.unit),
-                    td(null, null, m.place),
-                    td(null, null, new Date(m.time).toDateString())
+                    td(null, null, d.type),
+                    td(null, null, d.value),
+                    td(null, null, d.unit),
+                    td(null, null, new Date(d.time).toDateString())
                     )
                 })
             )
         )
     }    
 
+    function display_temperature(data) {
+        return `${data.value} ${data.unit} on ${new Date(data.time).toDateString()}`
+    }
+
+    function display_value(data) {
+        return `${data[0]} ${data[1]}`
+    }
+
+    function display_forecast(forecast) {
+        return table(null, 
+            thead(null, null, tr(null, null, td(null, "Time"), td(null, "Type"), td(null, "From"), td(null, "To"), td(null, "Unit"))),
+            tbody(null, null, ...forecast.map(f => {
+                return tr(null, null, 
+                    td(null, null, new Date(f.time).toDateString() + ' ' + new Date(f.time).getHours() + ':00:00'),
+                    td(null, null, f.type),
+                    td(null, null, f.from),
+                    td(null, null, f.to),
+                    td(null, null, f.unit)
+                    )
+                })
+            )
+        )
+    }
+
     return div(null, 
-        h2(null, 'Latest measurements'), 
+        h2(null, `Weather Report for ${city}`),
+        h3(null, 'Latest measurements'), 
         display_weather_data(model.latest_measurements()),
-        h2(null, 'Minimum temperature for the last 5 days'),
-        display_weather_data(model.min_temperature()),
-        h2(null, 'Maximum temperature for the last 5 days'),
-        display_weather_data(model.max_temperature())
+        h3(null, 'Last 5 days statistics'), 
+        p(null, 'Minimum temperature: ' + display_temperature(model.min_temperature())),
+        p(null, 'Maximum temperature: ' + display_temperature(model.max_temperature())),
+        p(null, 'Total precipitation: ' + display_value(model.total_precipitation())),
+        p(null, 'Average wind speed: ' + display_value(model.average_wind_speed())),
+        p(null, 'Dominant wind direction: ' + model.dominant_direction()),
+        p(null, 'Average cloud coverage: ' + display_value(model.average_cloud_coverage())),
+        h3(null, 'Hourly predictions for the next 24 hours'), 
+        display_forecast(model.hourly_predictions())
     )
 }
 
