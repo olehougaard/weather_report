cdead637bbc44bc66a237845fd0f497d8834fa19
Andreas <42655737+anonisnap@users.noreply.github.com>
2023-09-11 11:59:56 +0200

    Update Functions for individual weather aspects

--- static/view.js
+++ static/view.js
@@ -12,5 +12,39 @@ export default window => {
 
     const prompt = window.prompt.bind(window)
 
-    return { listen, prompt, displayError }
+    const update = (model) => {
+        console.log('> Updating Site')
+        // model.dump()
+        
+        // Update individual View Sections
+        let temp = model.temperature
+        updateTemperature(temp)
+
+        let perc = model.percipitation
+        updatePercipitation(perc)
+
+        let wind = model.wind_speed
+        updateWindSpeed(wind)
+
+        let cloud = model.cloud_coverage
+        updateCloudCoverage(cloud)        
+    }
+
+    function updateTemperature(t_data){
+        console.log('Temperature not updated')
+    }
+
+    function updatePercipitation(p_data){
+        console.log('Percipitation not updated')
+    }
+
+    function updateWindSpeed(w_data){
+        console.log('Wind Speed not updated')
+    }
+
+    function updateCloudCoverage(c_data){
+        console.log('Cloud Coverage not updated')
+    }
+
+    return { listen, prompt, displayError, update }
 }
