25a61dcdc13f6dfaa000357d61a624e6d9d15cee
MikRunRas <92924105+MikRunRas@users.noreply.github.com>
2023-09-20 16:42:51 +0200

    somethin aint right

--- static/model.js
+++ static/model.js
@@ -1,4 +1,19 @@
-export default model => {
+function currentModel(model) {
+    let temperature = model.temperature
+    let percipitation = model.percipitation
+    let wind_speed = model.wind
+    let cloud_coverage = model.cloud
+
+    return {
+        temperature,
+        percipitation,
+        wind_speed,
+        cloud_coverage,
+        dump
+    }
+}
+
+function forecastModel(model) {
     let temperature = model.temperature
     let percipitation = model.percipitation
     let wind_speed = model.wind
@@ -18,3 +33,5 @@ function dump() {
     // Using [this.] to ensure the data is the correct data in the object
     console.log(` ! MODEL DATA DUMP !\n\t${this.temperature}, ${this.percipitation}, ${this.wind_speed}, ${this.cloud_coverage}`)
 }
+
+export { currentModel, forecastModel }
--- static/view.js
+++ static/view.js
@@ -12,8 +12,8 @@ export default (window) => {
 
   const prompt = window.prompt.bind(window);
 
-  const update = (model) => {
-    console.log("> Updating Site");
+  const updateCurrent = (model) => {
+    console.log("> Updating Current");
     // model.dump()
 
     // Start Table Row
@@ -21,24 +21,24 @@ export default (window) => {
 
     // Update individual View Sections
     let temp = model.temperature;
-    updateTemperature(row, temp);
+    updateCurrentTemperature(row, temp);
 
     let wind = model.wind_speed;
-    updateWindSpeed(row, wind);
+    updateCurrentWindSpeed(row, wind);
     
     let perc = model.percipitation;
-    updatePercipitation(row, perc);
+    updateCurrentPercipitation(row, perc);
 
     let cloud = model.cloud_coverage;
-    // updateCloudCoverage(cloud)
+    // updateCurrentCloudCoverage(cloud)
 
-    // updateValueUnit(row, cloud);
+    // updateCurrentValueUnit(row, cloud);
 
     // End Table Row
     table_body.appendChild(row);
-  };
+    };
 
-  function updateTemperature(row, t_data) {
+    function updateCurrentTemperature(row, t_data) {
     console.log("Temperature is fucked");
 
     let td = document.createElement("td");
@@ -47,7 +47,7 @@ export default (window) => {
     row.appendChild(td);
   }
 
-  function updatePercipitation(row, p_data) {
+    function updateCurrentPercipitation(row, p_data) {
     console.log("Percipitation not updated");
 
     let td = document.createElement("td");
@@ -56,7 +56,7 @@ export default (window) => {
     row.appendChild(td);
   }
 
-  function updateWindSpeed(row, w_data) {
+    function updateCurrentWindSpeed(row, w_data) {
     console.log("Wind Speed not updated");
 
     let td = document.createElement("td");
@@ -65,7 +65,7 @@ export default (window) => {
     row.appendChild(td);
   }
 
-  function updateCloudCoverage(row, c_data) {
+    function updateCurrentCloudCoverage(row, c_data) {
     console.log("Cloud Coverage not updated");
 
     let td = document.createElement("td");
@@ -74,14 +74,78 @@ export default (window) => {
     row.appendChild(td);
   }
 
-  function updateValueUnit(row, data) {
+    function updateCurrentValueUnit(row, data) {
     // Create TD element
     let td = document.createElement("td");
     // Add Data Value and Unit to the Context
     td.textContent = `${data.value}${data.unit}`;
     // Add TD to the TR
     row.appendChild(td);
-  }
+    }
 
-  return { listen, prompt, displayError, update };
+    const updateForecast = (model) => {
+        console.log("> Updating Forecast");
+        // model.dump()
+
+        // Start Table Row
+        let row = document.createElement("tr");
+
+        // Update individual View Sections
+        let temp = model.temperature;
+        updateForecastTemperature(row, temp);
+
+        let wind = model.wind_speed;
+        updateForecastWindSpeed(row, wind);
+
+        let perc = model.percipitation;
+        updateForecastPercipitation(row, perc);
+
+        let cloud = model.cloud_coverage;
+        updateForecastCloudCoverage(cloud)
+
+        // updateForecastValueUnit(row, cloud);
+
+        // End Table Row
+        table_body.appendChild(row);
+    };
+
+    function updateForecastTemperature(row, t_data) {
+        console.log("Temperature is fucked");
+
+        let td = document.createElement("td");
+        td.textContent = `${t_data.from} - ${t_data.to} ${t_data.unit}`
+
+        row.appendChild(td);
+    }
+
+    function updateForecastPercipitation(row, p_data) {
+        console.log("Percipitation not updated");
+
+        let td = document.createElement("td");
+        td.textContent = `${p_data.from} - ${p_data.to} ${p_data.unit}`
+
+        row.appendChild(td);
+    }
+
+    function updateForecastWindSpeed(row, w_data) {
+        console.log("Wind Speed not updated");
+
+        let td = document.createElement("td");
+        td.textContent = `${w_data.from} - ${w_data.to} ${w_data.unit} , ${w_data.directions}`
+
+        row.appendChild(td);
+    }
+
+    function updateForecastCloudCoverage(row, c_data) {
+        console.log("Cloud Coverage not updated");
+
+        let td = document.createElement("td");
+        //td.textContent = `${c_data.from} - ${c_data.to} ${c_data.unit}`
+
+        //row.appendChild(td);
+    }
+
+  return { listen, prompt, displayError, updateCurrent, updateForecast };
 };
+
+
